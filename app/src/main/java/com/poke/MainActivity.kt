2d0629b76e673c43146ebf62ed4fc7f3d89493ac
ParkChan <hydrarkk@naver.com>
2020-07-22 21:09:53 +0900

    [작업내용] start!!

---
+++ app/src/main/java/com/poke/MainActivity.kt
@@ -0,0 +1,4 @@
+package com.poke
+
+class MainActivity {
+}
---
+++ app/src/main/java/com/poke/common/BaseFragment.kt
@@ -0,0 +1,35 @@
+package com.poke.common
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+
+abstract class BaseFragment<VDB : ViewDataBinding>(
+    @LayoutRes
+    private val resource: Int
+) : Fragment() {
+
+    protected lateinit var binding: VDB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate<VDB>(
+            inflater,
+            resource,
+            container,
+            false
+        ).apply{
+            lifecycleOwner = this@BaseFragment.viewLifecycleOwner
+        }
+        return binding.root
+    }
+}
